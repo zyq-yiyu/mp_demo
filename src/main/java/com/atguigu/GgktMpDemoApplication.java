372101276d048482ffe1bc67f7fca65190c46f53
xxx <ntzyq2021@163.com>
2022-12-21 12:56:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/GgktMpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgktMpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgktMpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atguigu/GgktMpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgktMpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
