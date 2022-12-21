7386eb7e17f63f82fd3353057596d6e243466c5e
xxx <ntzyq2021@163.com>
2022-12-21 13:16:50 +0800

    first

---
+++ src/main/java/com/atguigu/entity/User.java
@@ -0,0 +1,12 @@
+package com.atguigu.entity;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String email;
+}
